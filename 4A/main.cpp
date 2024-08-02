5b461ced5083a1ac1c4a67d197fa4074f801152b
Brett Sumser <bsumser@gmail.com>
2024-08-02 00:06:14 -0700

    accepted

---
+++ 4A/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int tc;
+
+    cin >> tc;
+
+    if (tc == 2) {
+        cout << "NO" << "\n";
+    }
+
+    else if (tc % 2 == 0) {
+        cout << "YES" << "\n";
+    }
+    else {
+        cout << "NO" << "\n";
+    }
+
+    return 0;
+}
