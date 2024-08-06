2ca7e8852b85772eae2b0b3ff527e0ea83c4c29e
Brett Sumser <bsumser@gmail.com>
2024-08-05 22:28:17 -0700

    deleted

--- template.cpp
+++ template.cpp
@@ -3,18 +3,6 @@
 using namespace std;
 
 int main() {
-    int tc;
-
-    cin >> tc;
-
-    while (tc--) {
-        string f, s;
-
-        cin >> f >> s;
-
-        swap(f[0], s[0]);
-
-        cout << f << " " << s << "\n";
-    }
+    
     return 0;
 }
